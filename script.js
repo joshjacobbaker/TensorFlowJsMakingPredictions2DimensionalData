31a54dadc027483416b95004326271d345da80ae
Josh Baker <josh.jacob.baker@gmail.com>
2021-11-01 19:27:17 -0700

    step-6 created trainModel function to compile, fit, and visualize error

--- script.js
+++ script.js
@@ -36,6 +36,14 @@ async function run() {
   // Create the model
   const model = createModel()
   tfvis.show.modelSummary({ name: "Model Summary" }, model)
+
+  // Convert the data to a form we can use for training.
+  const tensorData = convertToTensor(data)
+  const { inputs, labels } = tensorData
+
+  // Train the model
+  await trainModel(model, inputs, labels)
+  console.log("Done Training")
 }
 
 document.addEventListener("DOMContentLoaded", run)
@@ -92,5 +100,24 @@ function convertToTensor(data) {
       labelMax,
       labelMin,
     }
+  })
+}
+
+async function trainModel(model, inputs, labels) {
+  // Prepare the model for training.
+  model.compile({
+    optimizer: tf.train.adam(),
+    loss: tf.losses.meanSquaredError,
+    metrics: ["mse"],
+  })
+
+  const batchSize = 32
+  const epochs = 50
+
+  return await model.fit(inputs, labels, {
+    batchSize,
+    epochs,
+    shuffle: true,
+    callbacks: tfvis.show.fitCallbacks({ name: "Training Performance" }, ["loss", "mse"], { height: 200, callbacks: ["onEpochEnd"] }),
   })
 }
